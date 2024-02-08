01fd3f6eb7b85f12b63b546dec865c2cbb84ddef
ashishk2389 <85054040+ashishk2389@users.noreply.github.com>
2024-02-09 01:08:41 +0530

    Update ProductPageTest.java

--- src/test/java/com/swaglab/testscript/ProductPageTest.java
+++ src/test/java/com/swaglab/testscript/ProductPageTest.java
@@ -8,38 +8,39 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class ProductPageTest extends BaseClass {
-   ProductPage ProductPage;
-
-   @BeforeSuite
-   public void initilizeBroswer() {
-      initilization();
-      LoginPage loginPage = new LoginPage();
-      this.ProductPage = loginPage.Login("standard_user", "secret_sauce");
-   }
-
-   @Test
-   public void testAddToCartBackpack() {
-      Assert.assertFalse(this.ProductPage.addToCartBackpack().isDisplayed());
-   }
-
-   @Test
-   public void testAddToCartBikeLight() {
-      Assert.assertFalse(this.ProductPage.addToCartBikeLight().isDisplayed());
-   }
-
-   @Test
-   public void testAddToCartOnesie() {
-      Assert.assertFalse(this.ProductPage.addToCartOnesie().isDisplayed());
-   }
-
-   @Test
-   public void testAddToCartTShirt() {
-      Assert.assertFalse(this.ProductPage.addToCartTShirt().isDisplayed());
-   }
-
-   @Test
-   public void testAddToCartIcon() {
-      int itemsAddedToCart = Integer.parseInt(this.ProductPage.addToCartIcon.getText());
-      Assert.assertEquals((long)this.ProductPage.itemsAddedToCart, (long)itemsAddedToCart);
-   }
 }
+//    ProductPage ProductPage;
+
+//    @BeforeSuite
+//    public void initilizeBroswer() {
+//       initilization();
+//       LoginPage loginPage = new LoginPage();
+//       this.ProductPage = loginPage.Login("standard_user", "secret_sauce");
+//    }
+
+//    @Test
+//    public void testAddToCartBackpack() {
+//       Assert.assertFalse(this.ProductPage.addToCartBackpack().isDisplayed());
+//    }
+
+//    @Test
+//    public void testAddToCartBikeLight() {
+//       Assert.assertFalse(this.ProductPage.addToCartBikeLight().isDisplayed());
+//    }
+
+//    @Test
+//    public void testAddToCartOnesie() {
+//       Assert.assertFalse(this.ProductPage.addToCartOnesie().isDisplayed());
+//    }
+
+//    @Test
+//    public void testAddToCartTShirt() {
+//       Assert.assertFalse(this.ProductPage.addToCartTShirt().isDisplayed());
+//    }
+
+//    @Test
+//    public void testAddToCartIcon() {
+//       int itemsAddedToCart = Integer.parseInt(this.ProductPage.addToCartIcon.getText());
+//       Assert.assertEquals((long)this.ProductPage.itemsAddedToCart, (long)itemsAddedToCart);
+//    }
+// }
